487ff71d8d12b167e40a33c81aa2cacaf94faf26
Naimul Islam <ms0413354@gmail.com>
2023-05-01 17:00:56 +0600

    show all button and toggle dark and light

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ColorModeContext, useMode } from './thems';
+import Topbar from './pages/Navbar/Topbar';
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -8,7 +9,9 @@ function App() {
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<div className="app">
-					<main className="context"></main>
+					<main className="content">
+						<Topbar />
+					</main>
 				</div>
 			</ThemeProvider>
 		</ColorModeContext.Provider>
