89a373a324beedce4602f64c8966f4dca5aa87b0
Naimul Islam <ms0413354@gmail.com>
2023-05-01 15:23:44 +0600

    wrap context api from react and ThemeProvider from matrial ui in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './thems';
+
 function App() {
-	return <div className="App"></div>;
+	const [theme, colorMode] = useMode();
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+					<main className="context"></main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/pages/DashBord/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DashBord = () => {
+	return <div>Dashbord</div>;
+};
+
+export default DashBord;
