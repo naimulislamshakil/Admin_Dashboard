1276766d616b180d76358716b3cb050fdb7381a9
Naimul Islam <ms0413354@gmail.com>
2023-05-02 00:08:52 +0600

    complete show all contacts data

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -74,7 +74,7 @@ const Team = () => {
 	];
 	return (
 		<Box m="20px">
-			<Header title="TEAM" subTitle="Managing the Team Members" />
+			<Header title="TEAM" subTitle="Managing the Team Members." />
 
 			{/* CREATE A TABLE USING DATAGRID */}
 			<Box
